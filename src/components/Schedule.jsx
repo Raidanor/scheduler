1b75b1a544d06a626a1afec4cd9f510e77d86bbe
Raidanor <silverstallion30@gmail.com>
2024-02-02 03:51:49 +0400

    progress

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,6 +4,8 @@ import { supabase } from "../supabaseClient"
 //fullcalendar imports
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 import interactionPlugin from "@fullcalendar/interaction"
 
 //components
@@ -11,37 +13,32 @@ import interactionPlugin from "@fullcalendar/interaction"
 
 function Schedule()
 {
+    handleDateClick = (arg) =>
+    { // bind with an arrow function
+        alert(arg.dateStr)
+    }
 
     return(
         <>
             Schedule Place holder text
             <FullCalendar
-                plugins = { [ dayGridPlugin ]}
+                plugins = { [ dayGridPlugin, interactionPlugin]}
                 initialView = "dayGridMonth"
                 events =
                 {[
                     { title: "event 1", date: "2024-02-02"},
-                    { title: "event 1", date: "2024-02-03"},
+                    { title: "event 2", date: "2024-02-03"},
                     { title: "event 3", date: "2024-02-04"}
                     
                 ]}
-                eventContent = { renderEventContent }
+                dateClick={handleDateClick}
             />
             
         </>
     )
 }
 
-function renderEventContent(eventInfo)
-{
-    return(
-        <>
-            <b>{ eventInfo.timeText }</b>
-            <i>{ eventInfo.event.title }</i>
-            
-        </>
-    )
-}
+
 
 
 export default Schedule;
