d57c9c13e1024b591525ffe9fe4a6c5b9a28db02
siri1515 <siri91445@gmail.com>
2024-02-15 22:48:05 -0400

    multiple choice from employee list

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient'
+import { supabase } from '../supabaseClient';
+import Select from 'react-select';
 
 function Planner( { session } )
 {
@@ -9,7 +10,7 @@ function Planner( { session } )
 	const [date, setDate] = useState('');
 	const [startTime, setStartTime] = useState('');
 	const [endTime, setEndTime] = useState('');
-	const [selectedEmployee, setSelectedEmployee] = useState([]);
+	const [selectedEmployees, setSelectedEmployees] = useState([]);
 
 	useEffect(() => {
 		async function getUsernames()
@@ -25,18 +26,23 @@ function Planner( { session } )
 			}
             else
             {
-                setUsernameList(data);
+				const formattedData = data.map(user => ({
+                    value: user.username,
+                    label: user.username
+                }));
+                setUsernameList(formattedData);
             }
 		}
 
 		getUsernames();
 	}, [])
 
-
 	async function handleSubmit(e)
     {
 		e.preventDefault();
 	
+		const usernames = selectedEmployees.map(employee => employee.value);
+		console.log("usernames: ", usernames);
 		const { data, error } = await supabase
 			.from('events')
 			.insert([
@@ -46,7 +52,7 @@ function Planner( { session } )
 					date: date,
 					start_time: startTime,
 					end_time: endTime,
-					username: selectedEmployee
+					usernames: usernames
 				},
 			]);
 	
@@ -62,6 +68,13 @@ function Planner( { session } )
 			setDate('');
 		}
 	};
+
+	const customStyles = {
+		option: (provided) => ({
+		  ...provided,
+		  color: 'black',
+		}),
+	};
 	
     // geting the id of the signed in user
     const { user } = session
@@ -71,9 +84,73 @@ function Planner( { session } )
     // add to the env file and reead from there
     // could add tthem in array format as well and have multiple users have admin/upper level priviledges
 
-    // supabase function that checks user id
-    // rerube yes or no
+	return(
+		<div className="container mt-5 col-6">
+			<h2 className="mb-4">Add Shift</h2>
+			<form onSubmit={ handleSubmit }>
+				<div className="form-group">
+					<label>Title:</label>
+					<input
+						type="text"
+						className="form-control"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<label>Detail:</label>
+					<textarea
+					className="form-control"
+					value={detail}
+					onChange={(e) => setDetail( e.target.value )}
+					></textarea>
+				</div>
+				<div className="form-group">
+					<label>Date:</label>
+					<input
+						type="date"
+						className="form-control"
+						value={date}
+						onChange={(e) => setDate(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<label>Start Time:</label>
+					<input
+						type="time"
+						className="form-control"
+						value={startTime}
+						onChange={(e) => setStartTime(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<label>End Time:</label>
+					<input
+						type="time"
+						className="form-control"
+						value={endTime}
+						onChange={(e) => setEndTime(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<label>Employee:</label>
+					<Select
+						isMulti
+						name="employees"
+						options={usernameList}
+						className="basic-multi-select"
+						classNamePrefix="select"
+						onChange={(selectedOptions) => setSelectedEmployees(selectedOptions)}
+						value={selectedEmployees}
+						styles={customStyles}
+					/>
+				</div>
+				<button type="submit" className="btn btn-primary mt-5">Assign</button>
+			</form>
+		</div>
+	)
 
+	/*
 	return (user.id === "af37dc6f-0dfd-4f03-9b9f-ab4d05aec493") ? (     // this is just a very long ternary operator. I may factorise this later to make it more readable
 		<div className="container mt-5 col-6">
 			<h2 className="mb-4">Add Shift</h2>
@@ -128,13 +205,13 @@ function Planner( { session } )
 						className="form-control"
 						value={ selectedEmployee }
 						onChange={(e) => setSelectedEmployee(e.target.value)}
-                        
+
                         // to enable selection of multiple options
                         // multiple = "multiple"
 					>
 					<option value="">Choose the employee</option>
 					{usernameList.map((employee) => (
-						<option key={employee.id} value={employee.username} >
+						<option key={employee.id} value={employee.username}>
 							{employee.username}
 						</option>
 					))}
@@ -149,7 +226,7 @@ function Planner( { session } )
         <div>
             You do not have permission to access this page
         </div>
-    )
+    )*/
 };
 
 export default Planner;
