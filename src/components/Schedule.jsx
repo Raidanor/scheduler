ae30cb0c2bac25f5ea152152126e13ae2161a62f
siri1515 <siri91445@gmail.com>
2024-03-25 15:33:03 -0300

    deleted logic

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { supabase } from "../supabaseClient"
+import Modal from 'react-modal';
 
 //fullcalendar imports
 import FullCalendar from '@fullcalendar/react'
@@ -14,13 +15,27 @@ import interactionPlugin from "@fullcalendar/interaction"
 //packages
 import * as bootstrap from "bootstrap"
 
-function Schedule()
+Modal.setAppElement('#root');
+
+function Schedule( { session } )
 {
     // handleDateClick = (arg) =>
     // { // bind with an arrow function
     //     alert(arg.dateStr)
     // }
 	const [events, setEvents] = useState([]);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [eventToDelete, setEventToDelete] = useState(null);
+
+	const adminList = [
+	  "dc1a0207-c86f-43da-bee9-019c27352b0a",
+	  "255710a3-c965-437a-bc6b-03bbbd2b47df",
+	  "af37dc6f-0dfd-4f03-9b9f-ab4d05aec493",
+	  "ef1ebf8d-f25b-4030-8e81-d3e574f5128b"
+	];
+	console.log("session: ", session);
+
+
 
 	useEffect(() => {
 		async function getTask(){
@@ -46,6 +61,7 @@ function Schedule()
 			//console.log(data);
 	
 			setEvents(formattedEvents);
+			console.log("events: ", formattedEvents);
 
 		}
 
@@ -61,9 +77,39 @@ function Schedule()
 */
     function handleClickOnEvent( arg )
     {
-        alert("This event has been clicked")
+        //alert("This event has been clicked")
         console.log("title is: " + arg.event.title)
     }
+
+	
+	const handleDeleteEvent = async () => {
+		/*
+		if (!adminList.includes(session.user.id)) {
+			alert("Sorry, You don't have the permission");
+			return;
+		}*/
+		console.log("check: ", eventToDelete);
+		
+		const { error } = await supabase
+			.from("events")
+			.delete()
+			.match({ id: eventToDelete });
+		
+		if (error) {
+		  	alert("Error");
+		} else {
+			setEvents(events.filter(event => event.id !== eventToDelete));
+			console.log("new events:", events);
+			alert("Deleted!");
+		}
+		setModalIsOpen(false);
+	};
+
+	const openModalToDeleteEvent = (eventId) => {
+		setEventToDelete(eventId);
+		setModalIsOpen(true);
+	};
+
     return(
         <>
             <div className="m-2 pt-4">
@@ -87,7 +133,7 @@ function Schedule()
 								<p><strong>Detail:</strong> ${detail}</p>
 								<p><strong>Assigned to:</strong> ${usernames}</p>
 							`;
-                            return new bootstrap.Popover(info.el, 
+                            new bootstrap.Popover(info.el, 
                                 {
                                     title: info.event.title,
                                     placement:"auto",
@@ -96,6 +142,13 @@ function Schedule()
                                     content: content,
                                     html: true,
                                 })
+							if (adminList.includes(session.user.id)) {
+								let deleteButton = document.createElement('button');
+								deleteButton.innerText = 'Delete';
+								deleteButton.style.marginLeft = '5px';
+								deleteButton.onclick = () => openModalToDeleteEvent(info.event.id);
+								info.el.append(deleteButton);
+							}
                         }
                     }
                     headerToolbar = 
@@ -111,7 +164,36 @@ function Schedule()
                     // dateClick={handleDateClick}
                 />
             </div>
-            
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={() => setModalIsOpen(false)}
+				contentLabel="confirm delete"
+				style={{
+					content: {
+						top: '50%',
+						left: '50%',
+						right: 'auto',
+						bottom: 'auto',
+						marginRight: '-50%',
+						transform: 'translate(-50%, -50%)',
+						backgroundColor: '#FFF',
+						color: '#000',
+						padding: '20px',
+						border: '1px solid #CCC',
+						borderRadius: '4px',
+						zIndex: 1000
+					},
+					overlay: {
+						backgroundColor: 'rgba(0, 0, 0, 0.75)',
+						zIndex: 999
+					},
+				}}
+				>
+				<h2>Delete</h2>
+				<p>Are you sure you want to delete this event?</p>
+				<button onClick={handleDeleteEvent}>Yes</button>
+				<button onClick={() => setModalIsOpen(false)}>Cancel</button>
+			</Modal>
         </>
     )
 }
